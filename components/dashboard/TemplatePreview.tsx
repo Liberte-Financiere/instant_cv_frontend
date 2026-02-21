56f6ad40a67f04324481b7abc74d130070692402
Nerd658 <m9bikienga@gmail.com>
2026-02-21 23:29:29 +0000

    optimisation page templte et resout de vuln

--- components/dashboard/TemplatePreview.tsx
+++ components/dashboard/TemplatePreview.tsx
@@ -3,6 +3,8 @@ import { TemplateId } from '@/types/cv';
 import { DUMMY_CV } from './TemplateDummyData';
 import { CVThumbnail } from './CVThumbnail';
 
+import { LazyMount } from '@/components/ui/LazyMount';
+
 interface TemplatePreviewProps {
   templateId: TemplateId;
   color?: string;
@@ -22,7 +24,9 @@ export const TemplatePreview: React.FC<TemplatePreviewProps> = ({ templateId }) 
            We absolutely position it to match the previous layout.
        */}
        <div className="absolute top-0 left-0 w-full h-full flex justify-center">
-            <CVThumbnail cv={previewCV} scale={0.16} />
+            <LazyMount className="w-full h-full flex justify-center items-center">
+                <CVThumbnail cv={previewCV} scale={0.16} />
+            </LazyMount>
        </div>
     </div>
   );
